4bfef2612cfd187816c9a3f41c6bc26913285ddc
Uday-sidagana <uday.sidgana@gmail.com>
2024-11-09 08:09:10 +0530

    Connect the html, CSS and Javascript to the App

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request, url_for, render_template
 
-app= Flask(__name__)
+app= Flask(__name__, template_folder="template")
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def Ai():
-    return "HELLO"
+    if request.method == 'POST':
+
+        topic = request.form.get('AI')
+        return f"{topic}"
+    else:
+        return render_template("index.html")
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=7001, debug=True)
